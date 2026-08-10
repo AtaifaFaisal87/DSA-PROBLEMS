d5743ea3052f2bd3c6894ec51cb80d502663c65e
Ataifa Faisal <ataifafaisal2007@gmail.com>
2026-08-11 01:55:43 +0500

    Time: 0 ms (100%), Space: 7.9 MB (50.47%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -5,5 +5,14 @@ public:
 
       //the key to find this is binary system if we take 8 in binary and subtract -1 then apply and then the answer wont contain any 1 but if it is not power fo two there will be 1 
       //the comp already stores the int as binary so all we have to do is subtract 1 from them and apply and 
+
+      //or we can also do it
+    //   if(n<=0){
+    //         return false;
+    //     }
+    //     while(n%2==0){
+    //         n=n/2;
+    //     }
+    //     return n==1;
     }
 };
