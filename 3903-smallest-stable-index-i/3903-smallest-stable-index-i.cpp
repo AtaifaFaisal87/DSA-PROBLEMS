932c0257b70ab69e59bedc5f1f545e668bfd6153
Ataifa Faisal <ataifafaisal2007@gmail.com>
2026-09-05 20:26:20 +0500

    Time: 0 ms (100%), Space: 31 MB (72.89%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -29,3 +29,24 @@ public:
         return -1;
     }
 };
+
+//   int firstStableIndex(vector<int>& nums, int k) {
+//         int maxleft = INT_MIN;
+
+//         for (int i = 0; i < nums.size(); i++) {
+
+//             maxleft = max(maxleft, nums[i]);
+
+//             int minright = INT_MAX;
+
+//             for (int j = i; j < nums.size(); j++) {
+//                 minright = min(minright, nums[j]);
+//             }
+
+//             if (maxleft - minright <= k) {
+//                 return i;
+//             }
+//         }
+
+//         return -1;
+//     }
