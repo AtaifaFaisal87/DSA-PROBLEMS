094499b39ac07d1ada6bf8cc8baf5c210294e9c4
Ataifa Faisal <ataifafaisal2007@gmail.com>
2026-09-08 20:02:03 +0500

    Time: 0 ms (100%), Space: 8.4 MB (99.47%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,22 +1,32 @@
+// class Solution {
+// public:
+//     int countCommas(int n) {
+//         int total = 0;
+
+//         for (int j = 1; j <= n; j++) {
+//             string s = to_string(j);
+//             int count = 0;
+
+//             for (int i = s.length() - 1; i >= 0; i--) {
+//                 count++;
+
+//                 if (count == 3 && i != 0) {
+//                     total++;
+//                     count = 0;
+//                 }
+//             }
+//         }
+
+//         return total;
+//     }
+// };
+
 class Solution {
 public:
     int countCommas(int n) {
-        int total = 0;
+        if (n < 1000)
+            return 0;
 
-        for (int j = 1; j <= n; j++) {
-            string s = to_string(j);
-            int count = 0;
-
-            for (int i = s.length() - 1; i >= 0; i--) {
-                count++;
-
-                if (count == 3 && i != 0) {
-                    total++;
-                    count = 0;
-                }
-            }
-        }
-
-        return total;
+        return n - 999;
     }
 };
