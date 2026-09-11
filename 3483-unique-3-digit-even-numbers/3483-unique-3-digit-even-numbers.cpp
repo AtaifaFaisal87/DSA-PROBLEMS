be3443b7180cd7457f1cadd05646c11a88ac8d31
Ataifa Faisal <ataifafaisal2007@gmail.com>
2026-09-11 18:24:50 +0500

    Time: 4 ms (79.08%), Space: 29.9 MB (77.81%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
-
-        set<int> ans;
-
+        vector<int> ans;
         for (int i = 0; i < digits.size(); i++) {
             for (int j = 0; j < digits.size(); j++) {
                 for (int k = 0; k < digits.size(); k++) {
-
                     if (digits[i] == 0) {
                         continue;
                     }
-
                     if (i == j || j == k || i == k) {
                         continue;
                     }
-
                     if (digits[k] % 2 != 0) {
                         continue;
                     }
-
-                    ans.insert(digits[i] * 100 + digits[j] * 10 + digits[k]);
+                    ans.push_back(digits[i] * 100 + digits[j] * 10 + digits[k]);
                 }
             }
         }
-
+        sort(ans.begin(), ans.end());
+        ans.erase(unique(ans.begin(), ans.end()), ans.end());
         return ans.size();
     }
 };
